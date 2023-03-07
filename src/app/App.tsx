1691df83499cda71ba87db73aa80b5dfb35abe5c
Gryms <deshaies.guillaume@gmail.com>
2023-03-07 11:50:41 +0100

    feat: add basic ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useRouteError,
+} from "react-router-dom";
 
 import ArrivalRidePage from "../pages/ArrivalRidePage";
 import DepartRidePage from "../pages/DepartRidePage";
 
 import "./App.css";
 
+type RouteError = {
+  status: number;
+  statusText: string;
+  data: string;
+};
+
+const ErrorBoundary = () => {
+  const error = useRouteError() as RouteError;
+  return (
+    <>
+      <h1>{error.status}</h1>
+      <h3>{error.statusText}</h3>
+      <p>{error.data}</p>
+    </>
+  );
+};
+
 const router = createBrowserRouter([
-  { path: "/", element: <DepartRidePage /> },
+  { path: "/", element: <DepartRidePage />, errorElement: <ErrorBoundary /> },
   { path: "/arrival", element: <ArrivalRidePage /> },
 ]);
 
